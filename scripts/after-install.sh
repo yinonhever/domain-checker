733aaa2643847e65c5081a745a80d8c7c0b28db2
yinonhever <yinonehever@gmail.com>
2024-01-07 01:47:31 +0200

    stash before pull

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -4,6 +4,7 @@ cd /var/www/domain-checker
 git config --global --add safe.directory /var/www/domain-checker
 sudo chown -R root:ec2-user /var/www/domain-checker
 sudo chmod -R 775 /var/www/domain-checker
+sudo -u ec2-user git stash 
 sudo -u ec2-user git pull
 sudo -u ec2-user npm install
 sudo -u ec2-user npm run build 
