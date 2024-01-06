b463ebc7a1cf75c2e0756c95dbaa9302e9e9bf6b
yinonhever <yinonehever@gmail.com>
2024-01-07 01:25:50 +0200

    specify user before each postinstall command

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /var/www/domain-checker
-sudo git pull
-sudo npm install
-sudo npm run build 
+sudo -u ec2-user cd /var/www/domain-checker
+sudo -u ec2-user git pull
+sudo -u ec2-user npm install
+sudo -u ec2-user npm run build 
 sudo -u ec2-user /usr/local/bin/pm2 restart domain-checker
