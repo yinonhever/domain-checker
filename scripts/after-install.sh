30a04bda790544e812073cd0fd272fc85409c7ba
yinonhever <yinonehever@gmail.com>
2024-01-07 01:39:47 +0200

    attempt to fix permissions issue with build command

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd /var/www/domain-checker
+sudo chown -R root:ec2-user /var/www/domain-checker
+sudo chmod -R 775 /var/www/domain-checker
 sudo -u ec2-user git pull
 sudo -u ec2-user npm install
 sudo -u ec2-user npm run build 
