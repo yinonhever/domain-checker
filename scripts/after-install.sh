f3de1a4208e3d95227daf3eff782f801ca3ea1f9
yinonhever <yinonehever@gmail.com>
2024-01-07 02:07:16 +0200

    bring back chown/chmod commands

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -2,6 +2,8 @@
 
 cd /var/www/domain-checker
 git config --global --add safe.directory /var/www/domain-checker
+sudo chown -R root:ec2-user /var/www/domain-checker
+sudo chmod -R 775 /var/www/domain-checker
 sudo -u ec2-user git stash 
 sudo -u ec2-user git pull
 sudo -u ec2-user npm install
